b3e6708193ae30c78062d8ab2417c93fddadd882
Aya7m <yyoyo6987@gmail.com>
2025-09-13 23:52:25 +0300

    ready for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 const port = process.env.PORT 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const __dirname = path.resolve();
+app.use(express.json());
+
+// routes ...
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // ✅ catch-all route fix
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
